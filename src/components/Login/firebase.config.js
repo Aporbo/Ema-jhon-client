f3361038b7af7f4dd59f0961d8a78d9b4dab8fc4
Aporbo Ghosh <61919274+Aporbo@users.noreply.github.com>
2021-04-26 06:28:50 +0600

    first commit

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBfikpgvz9W_CenZMeOJfgVHax1Zv5MmsE",
+  authDomain: "ema-jhon-simple-8d62a.firebaseapp.com",
+  projectId: "ema-jhon-simple-8d62a",
+  storageBucket: "ema-jhon-simple-8d62a.appspot.com",
+  messagingSenderId: "816167480559",
+  appId: "1:816167480559:web:45ffedc29711472fa7bf1a"
+};
+  export default firebaseConfig;
